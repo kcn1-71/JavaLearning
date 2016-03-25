6d19ead1149ec4b69543260ecdd1d56f1bdd8916
kcn1-71 <kcn1.71@gmail.com>
2016-03-25 23:45:29 +0600

    QuickSort

--- QuickSort/src/MyQSort.java
+++ QuickSort/src/MyQSort.java
@@ -1,36 +1,31 @@
 
 public class MyQSort {
 
-	public static void QSort(int[] A, int left, int right) {
-		if (left >= right)
-			return;
+	public static void qSort(int[] A, int left, int right) {
 		int i = left, j = right;
-		int p = i - (i - j) / 2;
-		while (i < j) {
-			while ((A[i] <= A[p]) && (i < p)) {
+		int p = i + (j - i)/2;
+		while (i <= j) {
+			while (A[i] < A[p])
 				i++;
-			}
-			while ((A[p] <= A[j]) && (j > p)) {
+			while (A[p] < A[j])
 				j--;
-			}
-			if (i < j) {
+			if (i <= j) {
 				int temp = A[i];
 				A[i] = A[j];
 				A[j] = temp;
-				if (i == p)
-					p = j;
-				else if (j == p)
-					p = i;
-
+				i++;
+				j--;
 			}
 		}
-		QSort(A, left, p);
-		QSort(A, p + 1, right);
+		if (left < j)
+			qSort(A, left, j);
+		if (i < right)
+			qSort(A, i, right);
 	}
 
 	public static void main(String[] args) {
-		int[] Arr = new int[] { 2, 4, 1, 5, 3, 7, 9, 7, 3, 7, 8, 5, 4, 3, 9, 9, 6, 6 };
-		QSort(Arr, 0, Arr.length - 1);
+		int[] Arr = new int[] { 2, 4, 1, 5, 3, 7, 9, 7, 3, 7, 8, 5, 4, 3, 9, 9, 6, 6,0 };
+		qSort(Arr, 0, Arr.length - 1);
 	}
 
 }

f4cd397e9c45cc1484a20685da53cf435f1544f4
kcn1-71 <kcn1.71@gmail.com>
2016-04-03 07:39:15 +0600

    String to a text file

---
+++ ToATextFile/src/StringToAFile.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class StringToAFile {
+	public static void main(String[] args) {
+		try {
+			FileWriter writer = new FileWriter("Foo.txt");
+			writer.write("Hello Foo!");
+			writer.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}

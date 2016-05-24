f8d84cb52a5d04b7c8c97529cf804d14df0a2713
kcn1-71 <kcn1.71@gmail.com>
2016-05-25 00:49:01 +0600

    CourtCounter

--- Android/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Android/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        if (quantity > 0) quantity--;
+        display(quantity);
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        displayPrice(quantity);
     }
 
     /**
@@ -30,5 +43,23 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        int pricePerCup = 5;
+        String priceMessage = "Total price $" + quantity*pricePerCup + "\n" + "Thank you!";
+        displayMessage(priceMessage);
+
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
